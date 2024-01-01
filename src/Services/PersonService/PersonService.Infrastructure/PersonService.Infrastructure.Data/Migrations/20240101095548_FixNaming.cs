ed3576864a9a55031290cf71a82e29823f14b5c2
Ivan <Karausivan41@gmail.com>
2024-01-01 12:15:11 +0200

    Fix naming in migrations

---
+++ src/Services/PersonService/PersonService.Infrastructure/PersonService.Infrastructure.Data/Migrations/20240101095548_FixNaming.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PersonService.Infrastructure.Data.Migrations
+{
+    public partial class FixNaming : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "referrals",
+                newName: "id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "referrals",
+                newName: "Id");
+        }
+    }
+}
